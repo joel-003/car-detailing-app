4bb4aa01145151b656f3790416d74298bd4a1bd9
joeljoee <joelofficial003@gmail.com>
2026-02-04 21:47:48 +0530

    Add global error page and production-safe error handling

--- src/main/java/com/github/joel003/cardetailingapp/util/JPAUtil.java
+++ src/main/java/com/github/joel003/cardetailingapp/util/JPAUtil.java
@@ -3,12 +3,37 @@ package com.github.joel003.cardetailingapp.util;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JPAUtil {
 
-    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("carDetailingPU");
+    private static final EntityManagerFactory emf;
+
+    static {
+        try {
+            Map<String, String> props = new HashMap<>();
+
+            String dbUrl = System.getenv("DATABASE_URL");
+
+            if (dbUrl == null) {
+                throw new RuntimeException("DATABASE_URL not set");
+            }
+
+            props.put("javax.persistence.jdbc.url", dbUrl);
+            props.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
+
+            emf = Persistence.createEntityManagerFactory(
+                    "carDetailingPU", props
+            );
+
+        } catch (Exception e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
 
     public static EntityManager getEntityManager() {
         return emf.createEntityManager();
     }
 }
+
