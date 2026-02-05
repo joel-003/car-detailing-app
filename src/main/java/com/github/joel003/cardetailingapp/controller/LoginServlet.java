ac4a580c1bfabc4c1701e617a42f15a5cb0bed13
joeljoee <joelofficial003@gmail.com>
2026-02-05 16:26:29 +0530

    LoginServlet Changed back

--- src/main/java/com/github/joel003/cardetailingapp/controller/LoginServlet.java
+++ src/main/java/com/github/joel003/cardetailingapp/controller/LoginServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(value = "${pageContext.request.contextPath}/login")
+@WebServlet(value = "/login")
 public class LoginServlet extends HttpServlet {
 
     private final UserService userService;
